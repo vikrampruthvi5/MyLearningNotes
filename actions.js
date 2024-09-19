7d40e1b1d7fba5fd402eee51d70b7312cd695eac
Pruthvi Vikram <pvikram@tsys.com>
2024-09-19 12:24:03 -0400

    Adding HTML content files

---
+++ actions.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var testMessage = document.createElement("div");
+  testMessage.textContent = "This is a test message.";
+  document.body.appendChild(testMessage);
+});
